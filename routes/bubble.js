8faf8e95466727766b3e6947ce7cfe0228e68275
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-02 07:58:03 -1000

    Removed the unneeded swap variable

--- routes/bubble.js
+++ routes/bubble.js
@@ -1,8 +1,6 @@
 
 var array = [5, 8, 1, 4, 2, 9, 7, 6, 3];
 
-var swap = null;
-
 sort();
 
 function sort() {
@@ -10,7 +8,6 @@ function sort() {
     var first = array[i];
     var second = array[i + 1];
     if (array[i] > array[i + 1]) {
-      swap = true;
       first = array[i];
       second = array [i + 1];
       array.splice(i, 2, second, first)
@@ -18,3 +15,4 @@ function sort() {
     }
   }
 }
+console.log(array, 'array in loop');
