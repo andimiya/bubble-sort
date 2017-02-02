228a0268520f68b302cd4dcff27657bda05fe4c6
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-02 07:55:45 -1000

    Bubble sort part done

--- routes/bubble.js
+++ routes/bubble.js
@@ -1,27 +1,11 @@
 
-var array = [5, 1, 4, 2, 3];
+var array = [5, 8, 1, 4, 2, 9, 7, 6, 3];
 
 var swap = null;
 
-//First pass
-for (var i = 0; i <= array.length; i++) {
-  console.log('first pass');
-  var first = array[i];
-  var second = array[i + 1];
-  if (array[i] > array[i + 1]) {
-    swap = true;
-    first = array[i];
-    second = array [i + 1];
-    array.splice(i, 2, second, first)
-    console.log(array, 'array in loop');
-    console.log(first, 'first');
-    console.log(second, 'second');
-    console.log(swap, 'swap');
-  }
-}
+sort();
 
-if (swap === true){
-  console.log('second pass');
+function sort() {
   for (var i = 0; i <= array.length; i++) {
     var first = array[i];
     var second = array[i + 1];
@@ -30,48 +14,7 @@ if (swap === true){
       first = array[i];
       second = array [i + 1];
       array.splice(i, 2, second, first)
-      console.log(array, 'array in loop');
-      console.log(first, 'first');
-      console.log(second, 'second');
+      sort();
     }
   }
-  swap = false;
 }
-
-if (swap === true){
-  console.log('third pass');
-  for (var i = 0; i <= array.length; i++) {
-    var first = array[i];
-    var second = array[i + 1];
-    if (array[i] > array[i + 1]) {
-      swap = true;
-      first = array[i];
-      second = array [i + 1];
-      array.splice(i, 2, second, first)
-      console.log(array, 'array in loop');
-      console.log(first, 'first');
-      console.log(second, 'second');
-    }
-  }
-  swap = false;
-}
-
-if (swap === true){
-  console.log('fourth pass');
-  for (var i = 0; i <= array.length; i++) {
-    var first = array[i];
-    var second = array[i + 1];
-    if (array[i] > array[i + 1]) {
-      swap = true;
-      first = array[i];
-      second = array [i + 1];
-      array.splice(i, 2, second, first)
-      console.log(array, 'array in loop');
-      console.log(first, 'first');
-      console.log(second, 'second');
-    }
-  }
-  swap = false;
-}
-
-console.log(array, 'last array');
