01f22ce6fe9ce86668b3a0c535c5f0c9c522d544
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-02 07:18:05 -1000

    Bubble sort works, need to optize now to make sure it works for an infinite number of array lengths

--- routes/bubble.js
+++ routes/bubble.js
@@ -1,9 +1,77 @@
-console.log('WORKS');
 
-var start = [5, 1, 4, 2, 8];
+var array = [5, 1, 4, 2, 3];
 
-let length = start.length;
+var swap = null;
 
-// for (var i = 0, i < start.length, i++){
+//First pass
+for (var i = 0; i <= array.length; i++) {
+  console.log('first pass');
+  var first = array[i];
+  var second = array[i + 1];
+  if (array[i] > array[i + 1]) {
+    swap = true;
+    first = array[i];
+    second = array [i + 1];
+    array.splice(i, 2, second, first)
+    console.log(array, 'array in loop');
+    console.log(first, 'first');
+    console.log(second, 'second');
+    console.log(swap, 'swap');
+  }
+}
 
-// }
+if (swap === true){
+  console.log('second pass');
+  for (var i = 0; i <= array.length; i++) {
+    var first = array[i];
+    var second = array[i + 1];
+    if (array[i] > array[i + 1]) {
+      swap = true;
+      first = array[i];
+      second = array [i + 1];
+      array.splice(i, 2, second, first)
+      console.log(array, 'array in loop');
+      console.log(first, 'first');
+      console.log(second, 'second');
+    }
+  }
+  swap = false;
+}
+
+if (swap === true){
+  console.log('third pass');
+  for (var i = 0; i <= array.length; i++) {
+    var first = array[i];
+    var second = array[i + 1];
+    if (array[i] > array[i + 1]) {
+      swap = true;
+      first = array[i];
+      second = array [i + 1];
+      array.splice(i, 2, second, first)
+      console.log(array, 'array in loop');
+      console.log(first, 'first');
+      console.log(second, 'second');
+    }
+  }
+  swap = false;
+}
+
+if (swap === true){
+  console.log('fourth pass');
+  for (var i = 0; i <= array.length; i++) {
+    var first = array[i];
+    var second = array[i + 1];
+    if (array[i] > array[i + 1]) {
+      swap = true;
+      first = array[i];
+      second = array [i + 1];
+      array.splice(i, 2, second, first)
+      console.log(array, 'array in loop');
+      console.log(first, 'first');
+      console.log(second, 'second');
+    }
+  }
+  swap = false;
+}
+
+console.log(array, 'last array');
